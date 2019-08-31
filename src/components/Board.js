b5c0d42cd2b7669c64d6cc45803940d8ccb7ecea
Seth Lachman <lachmanseth17@gmail.com>
2019-08-31 15:57:58 -0400

    Implemented turn taking.

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,18 +3,20 @@ import Square from "./Square";
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   const handleClick = i => {
     const newSquares = squares.slice();
-    newSquares[i] = "X";
+    newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   };
 
   const renderSquare = i => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   };
 
-  const status = "Next player: X";
+  const status = `Next player: ${xIsNext ? "X" : "O"}`;
 
   return (
     <div>
